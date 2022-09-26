864969edc9f1d51981571f54d11ece0f517420f7
Raj <varasaniraj5@gmail.com>
2022-09-27 01:42:04 +0530

    Updated constants

--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -1,4 +1,6 @@
-const BASE_URL = "https://indie-foods.centralindia.cloudapp.azure.com/api/v1";
+// const BASE_URL = "https://indie-foods.centralindia.cloudapp.azure.com/api/v1";
+const BASE_URL =
+  process.env.REACT_APP_BACKEND_URL || "http://localhost:8000/api/v1";
 const AUTH_URL = `${BASE_URL}/auth`;
 const USER_URL = `${BASE_URL}/user`;
 const CHEF_URL = `${BASE_URL}/chef`;
@@ -14,6 +16,7 @@ export const UPDATE_ADDRESS_URL = `${USER_URL}/updateAddress`;
 export const UPDATE_MENU_ITEM_URL = `${CHEF_URL}/menuItems/`;
 export const UPDATE_PROFILE_PICTURE_URL = `${CHEF_URL}/updateProfilePicture`;
 export const UPDATE_PROFILE_DATA_URL = `${CHEF_URL}/updateProfileData`;
+export const SET_SIGN_CONTRACT_SUCCESS_URL = `${CHEF_URL}/setSignedContractSuccess`;
 export const FETCH_MENU_ITEMS_URL = `${MENU_ITEMS_URL}/`;
 export const INITIALIZE_ORDER_URL = `${ORDER_URL}/initializeOrder`;
 export const VERIFY_ORDER_URL = `${ORDER_URL}/verifyOrder`;
